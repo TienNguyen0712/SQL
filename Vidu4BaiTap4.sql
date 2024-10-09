f0365f4bfcb1c75b24f835c152176c4b702d68e3
Nguyễn Đăng Tiến <131792899+TienNguyen0712@users.noreply.github.com>
2024-10-09 23:29:22 +0700

    Add files via upload

---
+++ Vidu4BaiTap4.sql
@@ -0,0 +1,30 @@
+-- Tim gia tri thap nhat cua cac san pham torng bang "Products"--
+--Vidu1--
+SELECT MIN (UnitPrice) AS "Min_Price" --Dat ten thay the cho cot la "Min_Price"
+FROM dbo.Products;
+--Lay ra ngay dat hang gan day nhat tu bang "Orders"--
+--VIdu2--
+SELECT MAX (OrderDate) AS "Ngay dat hang gan day"
+FROM dbo.Orders;
+--Tim raso luong hang ton kho nhieu nhat--
+--Vidu3--
+SELECT MAX (UnitsInStock) AS "So luong hang ton kho nhieu nhat"
+FROM dbo.Products;
+--Tim ra tuoi doi cua nhan vien lon nhat trong cong ty--
+--Ngay thang nam sinh nho nhat la tuoi lon nhat--
+--BaiTap1--
+SELECT MIN (BirthDate) AS "Ngay thang nam it nhat cua nhan vien"
+FROM dbo.Employees;
+--Tim ra tuoi cua nhan vien nho nhat trong cong ty--
+--Ngay thang nam sinh lon nhat trong cong ty--
+--BaiTap2--
+SELECT MAX (BirthDate) AS "Ngay thang nam lon nhat cua nhan vien"
+FROM dbo.Employees;
+--Tim ra ngay Orders som nhat cua san pham--
+--BaiTap3--
+SELECT MAX (OrderDate) AS "Ngay som nhat"
+FROM dbo.Orders;
+--Tim ra ngay Giao hang lau nhat cua san pham--
+--BaiTap4--
+SELECT MIN (ShippedDate) AS "Ngay giao hang lau nhat"
+FROM dbo.Orders;
